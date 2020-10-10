7f1468f3c4dcac4ceb5ab7fcd912f285dedec559
Yocuef Ramoul <youceframh@gmail.com>
2020-10-10 21:51:10 +0200

    task 4 2/2 completed

--- app/Http/Controllers/profilepic.php
+++ app/Http/Controllers/profilepic.php
@@ -19,7 +19,25 @@ class profilepic extends Controller
 
     public function post(Request $request){
 
-     
+        $validate = $request->validate([
+            'profilepic' => ['required','file','max:4096'],
+        ]);
+
+        $file = $request->profilepic;
+       $imgid = uniqid("picture");
+     $filemime = $file->extension();
+
+       $newpth =  $file->move(storage_path('/app/public'),$imgid.".".$filemime);
+       $vv = $imgid.".".$filemime;
+        $insert =  DB::table('users')->where('id',Auth::user()->id)->update(['picture' => "$vv"]);
+
+        
+
+        if($insert){
+            return view('dashboard-profile',['success' => 'تم التغيير بنجاح']);
+        }else{
+            return view('dashboard-profile',['success' => 'لا يمكن التغيير حاليا حاول لاحقا']);
+        }
     }
 
 }
