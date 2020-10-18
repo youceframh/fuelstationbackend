8dc126dee0010479b27cf1c488502c33a820db20
Yocuef Ramoul <youceframh@gmail.com>
2020-10-18 16:51:37 +0200

    securing delegates registrations route and making privilages

--- app/Http/Controllers/registerdelegate.php
+++ app/Http/Controllers/registerdelegate.php
@@ -10,7 +10,7 @@ class registerdelegate extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth.web','company.isverified']);
     }
 
     public function get(){
