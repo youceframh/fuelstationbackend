9b03ae7b01fd68fab840003a0cda611e89517cad
Yocuef Ramoul <youceframh@gmail.com>
2020-10-31 15:02:21 +0100

    patrol addition middleware and time of saving

--- app/Http/Controllers/patroladd.php
+++ app/Http/Controllers/patroladd.php
@@ -10,7 +10,7 @@ class patroladd extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth'); 
+        $this->middleware('teamleader'); 
     }
     public function get(){
         //date("Y-m-d", time() - 60 * 60 * 24);
--- app/Http/Middleware/annexteamleaderemployee.php
+++ app/Http/Middleware/annexteamleaderemployee.php
@@ -3,11 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use DB;
-use Auth;
 
-
-class checkifteamleader
+class annexteamleaderemployee
 {
     /**
      * Handle an incoming request.
@@ -18,10 +15,6 @@ class checkifteamleader
      */
     public function handle($request, Closure $next)
     {
-        $get_user_type = Auth::user()->typeofuser;
-        if($get_user_type != 'annex_TL'){
-            return redirect('/dashboard');
-        }
         return $next($request);
     }
 }
