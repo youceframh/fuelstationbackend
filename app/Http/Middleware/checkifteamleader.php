0b871610964a541d1c25cd6a1b89986ab2986f6f
Yocuef Ramoul <youceframh@gmail.com>
2020-10-31 22:35:29 +0100

    patrol finalized

--- app/Http/Middleware/checkifteamleader.php
+++ app/Http/Middleware/checkifteamleader.php
@@ -18,10 +18,26 @@ class checkifteamleader
      */
     public function handle($request, Closure $next)
     {
-        $get_user_type = Auth::user()->typeofuser;
-        if($get_user_type != 'annex_TL'){
-            return redirect('/dashboard');
+        if(Auth::check()){
+            $get_user_type = Auth::user()->typeofuser;
+            $get_user_email= Auth::user()->email;
+            $get_user_time = DB::table('employees')->where('email',$get_user_email)->first();
+            $employee_starttime = date('H:i',strtotime($get_user_time->patrol_time_start));
+            $employee_endtime = date('H:i',strtotime($get_user_time->patrol_time_end));
+    
+            if($get_user_type == 'annex_TL'){
+                if(date('H:i') >= $employee_endtime) {
+                    return redirect('/patrol/show');
+                  }elseif(date('H:i') < $employee_starttime){
+                    return redirect('/patrol/show');
+                  }
+                  return $next($request);
+            }else{
+                return redirect('/dashboard');
+            }
+        }else{
+            return redirect('/login');
         }
-        return $next($request);
+    
     }
 }
